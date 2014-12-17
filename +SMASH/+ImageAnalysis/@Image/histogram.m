699c182d2003a97ec307e89705d455a8c86c709d
Tommy Ao <tao@sandia.gov>
2014-12-17 09:25:21 -0700

    Changed Image method 'hist' to 'histogram'

--- +SMASH/+ImageAnalysis/@Image/histogram.m
+++ +SMASH/+ImageAnalysis/@Image/histogram.m
@@ -1,23 +1,26 @@
-% HIST Histogram of Image data 
+% HISTOGRAM Histogram of Image data 
 %
 % This method provide a graphical representation of the distribution of 
 % Image data. Elements in data are sorted into 'nbins' of equally spaced 
 % bins along the x-axis between the minimum and maximum values of data.
 %
 % Usage:
-%   >> varargout=hist(object,varargin);
+%   >> varargout=histogram(object,varargin);
 % The output variables are 'nelements', the number of elements in each bin, 
-% and 'xcenters', the centers of each bin.
+% and 'xcenters', the centers of each bin.  A default of 10 bins is used,
+% but may be changed by input a value for 'nbins'.
+% For example, histogram with 100 bins:
+%   >> [nelements,xcenters] = histogram(object,100);
 % Specifying no output variables create histogram bar plot of object data:
-%   >> hist(object,varargin);
+%   >> histogram(object,varargin);
 % 
 %
 % See also IMAGE
 
-% created December 16, 2014 by Tommy Ao (Sandia National Laboratories)
+% created December 17, 2014 by Tommy Ao (Sandia National Laboratories)
 
 %
-function varargout=hist(object,varargin)
+function varargout=histogram(object,varargin)
 
 varargout=cell(1,nargout);
 [varargout{:}]=hist(object.Data(:),varargin{:});    
