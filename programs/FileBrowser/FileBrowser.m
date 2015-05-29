037aa8e0eae8f197c006a1c7bd124b117c0e8ad7
Daniel Dolan <dhdolan@sandia.gov>
2015-05-29 12:45:21 -0600

    Still working on FileBrowser

--- programs/FileBrowser/FileBrowser.m
+++ programs/FileBrowser/FileBrowser.m
@@ -1,7 +1,14 @@
 % FileBrowser Interactive file browser
 function varargout=FileBrowser(varargin)
 
-fig=CreateDialog;
+% create dialog if it doesn't already exist
+h=findobj(0,'Type','figure','Tag','SMASH:FileBrowser');
+if ishandle(h)
+    figure(h);
+    fig=getappdata(h,'DialogObject');
+else
+    fig=createDialog;
+end
 
 % manage output
 if nargout>0
