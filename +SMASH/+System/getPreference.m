37e5101d7151798bd10382a7ec0a61782f0f132f
Daniel Dolan <dhdolan@sandia.gov>
2015-11-11 12:08:12 -0700

    Added different persistence levels to the toolbox's preference management.

--- +SMASH/+System/getPreference.m
+++ +SMASH/+System/getPreference.m
@@ -11,31 +11,74 @@
 %    getPreference(...);
 %
 %
-% NOTE: preference settings are persistent within and between MATLAB
-% sessions.
-%
 % See also System, setPreference
 %
 
 %
 % created November 11, 2015 by Daniel Dolan (Sandia National Laboratories)
 %
-function varargout=getPreference(name)
+function varargout=getPreference(name,mode)
 
 % manage input
+if (nargin<2) || isempty(mode)
+    mode='alert';
+end
+
+% search preferences
+type='';
 if nargin==0
-    value=getpref('SMASH');
+    value.Workspace=WorkspacePreference();
+    value.Session=SessionPreference();
+    value.Permanent=PermanentPreference();
 else
-    assert(isvarname(name) && ispref('SMASH',name),...
-        'ERROR: invalid preference name');
-    value=getpref('SMASH',name);   
+    assert(isvarname(name),'ERROR: invalid preference name');
+    try
+        value=WorkspacePreference(name);
+        type='workspace';
+    catch
+        try
+            value=SessionPreference(name);
+            type='session';
+        catch
+            try
+                value=PermanentPreference(name);
+                type='permanent';
+            catch
+                if strcmpi(mode,'alert')
+                    error('ERROR: requested preference is not defined');
+                else
+                    value=[];
+                end
+            end
+        end
+    end
 end
 
 % manage output
 if nargout==0
-    disp(value);
+    if nargin==0
+        fprintf('Workspace preferences:\n');
+        printStructure(value.Workspace);
+        fprintf('Session preferences:\n');
+        printStructure(value.Session);
+        fprintf('Permanent preferences:\n');
+        printStructure(value.Permanent)
+    else
+        disp(value);
+    end
 else
     varargout{1}=value;
+    varargout{2}=type;
+end
+
+end
+
+function printStructure(data)
+
+if isempty(fieldnames(data))
+    fprintf('\n');
+else
+    disp(data);
 end
 
 end
--- +SMASH/+System/setPreference.m
+++ +SMASH/+System/setPreference.m
@@ -1,11 +1,21 @@
 % setPreference Set toolbox preference(s)
 %
-% This function sets preferences used by the SMASH toolbox.  Preferences
-% are defined by name/value pairs.
-%    setPreference(name1,value1,...);
+% This function sets preferences used by the SMASH toolbox.
+%    setPreference(name,value);
+% The input "name" must be a valid variable name: character strings
+% beginning with a letter containing letters, numbers, and underscores only
+% (case sensitive).  The input "value" can be any MATLAB variable
+% understood by a particular feature in the toolbox.
 %
-% NOTE: preference settings are persistent within and between MATLAB
-% sessions.
+% Several levels of preference persistence can be specified.  
+%    setPreference(name,value,'workspace'); % default choice
+%    setPreference(name,value,'session');
+%    setPreference(name,value,'permanent');
+% Workspace preferences remain in memory until the primary workspace is
+% reset, typically by the "clear all" command.  Session preferences persist
+% until the end of the current MATLAB session, while permanent preferences
+% carry over to future sessions.  Persistance can be changed at any time by
+% overwritting the preference value.
 % 
 % See also System, getPreferences
 %
@@ -13,17 +23,33 @@
 %
 % created November 11, 2015 by Daniel Dolan (Sandia National Laboratories)
 %
-function setPreference(varargin)
+function setPreference(name,value,persist)
 
 % manage input
-assert(rem(nargin,2)==0,'ERROR: unmatched name/value pair');
+assert(nargin>=2,'ERROR: insufficient input');
+assert(isvarname(name),'ERROR: invalid preference name');
 
-% store preferences in a group called SMASH
-for n=1:2:nargin
-    name=varargin{n};
-    assert(isvarname(name),'ERROR: invalid preference name');
-    value=varargin{n+1};
-    setpref('SMASH',name,value);
+if (nargin<3) || isempty(persist)
+    persist='session';
+end
+assert(ischar(persist),'ERROR: invalid persist value');
+
+% store preference
+switch lower(persist)
+    case 'workspace'
+        WorkspacePreference(name,value);
+        SessionPreference(name,[],'remove');
+        PermanentPreference(name,[],'remove');    
+    case 'session'
+        WorkspacePreference(name,[],'remove');
+        SessionPreference(name,value);
+        PermanentPreference(name,[],'remove');        
+    case 'permanent'
+        WorkspacePreference(name,[],'remove');
+        SessionPreference(name,[],'remove');
+        PermanentPreference(name,value);      
+    otherwise
+        error('ERROR: invalid persist value');
 end
 
 end
