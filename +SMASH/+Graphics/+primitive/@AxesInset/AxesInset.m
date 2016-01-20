f7146af8fc4bba8fbfb4989d58e7b5af86c6aabd
Daniel Dolan <dhdolan@sandia.gov>
2016-01-19 21:05:02 -0700

    Substantially revised AxesInset (Graphics package)    -Changed from a single-use function to handle class object (with simple front end)    -Interactive and manual adjustment of the inset now supported

---
+++ +SMASH/+Graphics/+primitive/@AxesInset/AxesInset.m
@@ -0,0 +1,108 @@
+% This class creates and manages axes insets, which are useful for
+% highlighting details in a plot.
+%
+% Insets are generated from a bounded region of a source axes.  A rectangle
+% is drawn on the source axes to indicate the inset bounds, and all
+% graphical content in these bounds is reproduced in a separte (inset) axes.
+% Horizontal/vertical boundaries are specified in axes units; inset axes
+% position is specified in normalized units (with respect to the source
+% axes).
+%
+
+%
+% created January 19, 2016 by Daniel Dolan (Sandia National Laboratories)
+%
+classdef AxesInset < handle
+    %%
+    properties (SetAccess = protected)
+        Source % Source axes handle
+        Inset % Inset axes handle
+        Rectangle % Inset rectangle handle
+    end
+    properties
+        XBound % Horizontal bounds
+        YBound % Vertical bounds
+        Position = [0.70 0.05 0.25 0.2] % normalized inset position
+    end
+    %%
+    methods (Hidden=true)
+        function object=AxesInset(varargin)
+            object=processInput(object,varargin{:});                                   
+            update(object);
+        end
+    end
+    %%
+    methods
+        function delete(object)            
+            if ishandle(object.Inset)
+                delete(object.Inset);
+            end            
+            if ishandle(object.Rectangle)
+                delete(object.Rectangle)
+            end            
+        end
+    end
+    %% setters
+    methods
+        function set.XBound(object,value)
+            assert(isnumeric(value) && numel(value)==2,...
+                'ERROR: invalid XBound value');
+            value=sort(value);
+            assert(diff(value)>0,'ERROR: invalid XBound value');
+            object.XBound=value;
+        end
+        function set.YBound(object,value)
+            assert(isnumeric(value) && numel(value)==2,...
+                'ERROR: invalid YBound value');
+            value=sort(value);
+            assert(diff(value)>0,'ERROR: invalid YBound value');
+            object.YBound=value;
+        end
+        function set.Position(object,value)
+            assert(isnumeric(value) && numel(value)==4,...
+                'ERROR: invalid Position value');
+            assert(all(value(3:4)>0),'ERROR: invalid Position value');
+            object.Position=value;
+        end
+    end
+end
+
+%%
+function object=processInput(object,varargin)
+
+% determine source handle
+if (nargin<2) || ischar(varargin{1})
+    object.Source=gca;
+elseif ishandle(varargin{1})
+    type=get(varargin{1},'Type');
+    assert(strcmpi(type,'axes'),'ERROR: invalid source handle');
+    object.Source=varargin{1};
+    varargin=varargin(2:end);
+else
+    error('ERROR: invalid source handle');
+end
+
+% process options
+Narg=numel(varargin);
+assert(rem(Narg,2)==0,'ERROR: unmatched name/value pair');
+
+object.XBound=xlim(object.Source);
+object.YBound=ylim(object.Source);
+for n=1:2:Narg
+    name=varargin{n};
+    assert(ischar(name),'ERROR: invalid property name');
+    name=lower(name);
+    value=varargin{n+1};
+    switch name
+        case {'xbound' 'xlim'}
+            object.XBound=value;
+        case {'ybound' 'ylim'}
+            object.YBound=value;
+        case 'position'
+            object.Posiiton=value;
+        otherwise
+            fprintf('Ignoring unrecognized name "%s"\n',name);
+    end
+end
+
+end
--- +SMASH/+Graphics/AxesInset.m
+++ +SMASH/+Graphics/AxesInset.m
@@ -4,113 +4,34 @@
 % that emphasizes a particular region of interest.  The horizontal and
 % vertical limits of the inset and its position (with respect) to the
 % original can be specified.
-%
-% Usage:
-% >>[hinset,hrectangle]=AxesInset(haxes,'setting',value,...);
-%
+%    AxesInset(haxes,name,value,...);
 % If the original axes handle (haxes) is omitted, the function defaults to
-% the current axes (as defined by gca).  The following settings can be
-% specified.
-%   'xlim'     : horizontal range of the inset
-%   'ylim'     : vertical range of the inset
-%   'position' : inset position  [x0 y0 Lx Ly] relative to the original
-%   'boundary' : mark inset ('on') position on original axes
+% the current axes.  Valid setting names include:
+%   'xbound'     : horizontal bounds of the inset
+%   'ybound'     : vertical bounds of the inset
+%   'position'   : inset axes position [x0 y0 Lx Ly] relative to the original
 %
-% Example
-% figure; line % create a basic axes
-% hinset=AxesInset('xlim',[0.1 0.2],'ylim',[0.1 0.2]);
+% Inset bounds can be controlled by click/dragging on the rectangle in the
+% original axes.  To manually adjust the bounds or the inset axes position,
+% right-click on the rectangle and select "Position inset".  The function
+% also returns a custom object for advanced inset control.
+%    object=AxesInset(...);
+%    object.XBound=[a b];
+%    update(object);
+%
+% See also Graphics, SMASH.Graphics.primitive.AxesInset
+%
 
-% updated 8/18/2011 by Daniel Dolan
+%
+% created January 19, 2016 by Daniel Dolan (Sandia National Laboratories)
+%
 function varargout=AxesInset(varargin)
 
-%%%%%%%%%%%%%%%
-% input check %
-%%%%%%%%%%%%%%%
-if (nargin==0) || ischar(varargin{1})
-    haxes=gca;
-elseif ishandle(varargin{1})
-    haxes=varargin{1};
-    varargin=varargin(2:end);
-else
-    error('ERROR: invalid first input');
+object=SMASH.Graphics.primitive.AxesInset(varargin{:});
+
+% manage output
+if nargout>0
+    varargout{1}=object;
 end
 
-if rem(numel(varargin),2)~=0
-    error('Unmatched pair of input arguments')
-end
-
-% default settings
-xlimits=xlim(haxes);
-ylimits=ylim(haxes);
-pos=[0.70 0.05 0.25 0.25];
-boundary='on';
-
-% sweep through remaining inputs
-for ii=1:2:numel(varargin)
-    name=lower(varargin{ii});
-    value=varargin{ii+1};
-    switch name
-        case 'xlim'
-            xlimits=value;
-        case 'ylim'
-            ylimits=value;
-        case 'position'
-            pos=value;
-        case boundary
-            boundary=value;
-        otherwise
-            fprintf('Unrecognized input ''%s'' ingnored',name);
-    end
-end
-
-if isempty(xlimits)
-    error('No xlimits were given!');
-end
-if isempty(ylimits)
-    error('No ylimits were given!');
-end
-
-%%%%%%%%%%%%%%%%
-% create inset %
-%%%%%%%%%%%%%%%%
-fig=ancestor(haxes,'figure');
-hinset=copyobj(haxes,fig);
-set(hinset,'Color','none','XLim',xlimits,'YLim',ylimits,'Box','on')
-
-% position inset
-mainpos=get(haxes,'Position');
-insetpos(1)=mainpos(1)+pos(1)*mainpos(3);
-insetpos(2)=mainpos(2)+pos(2)*mainpos(4);
-insetpos(3)=mainpos(3)*pos(3);
-insetpos(4)=mainpos(4)*pos(4);
-set(hinset,'Position',insetpos);
-
-% delete extraneous labels from inset
-delete(get(hinset,'Xlabel'));
-delete(get(hinset,'Ylabel'));
-delete(get(hinset,'Title'));
-ht=findall(hinset,'Type','text');
-delete(ht);
-
-% draw inset box on original axes
-if strcmpi(boundary,'on')
-    boxpos(1)=xlimits(1);
-    boxpos(2)=ylimits(1);
-    boxpos(3)=diff(xlimits);
-    boxpos(4)=diff(ylimits);
-    hrectangle=rectangle('Parent',haxes,'Position',boxpos,...
-        'LineStyle','--');
-else
-    hrectangle=[];
-end
-
-%%%%%%%%%%%%%%%%%
-% handle output %
-%%%%%%%%%%%%%%%%%
-if nargout>=1
-    varargout{1}=hinset;
-end
-
-if nargout>=2
-    varargout{2}=hrectangle;
 end
