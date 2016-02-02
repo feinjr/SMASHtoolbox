3a6bdf02eb511a423f7442e10c0200cfd289c2a6
pfknapp <pfknapp@sandia.gov>
2016-02-02 07:57:20 -0700

    Fixed a bug in the measure temperature method that was causing artifacts in the fitting procedure

--- +SMASH/+Nuclear/@NTOF/report.m
+++ +SMASH/+Nuclear/@NTOF/report.m
@@ -7,7 +7,7 @@ Narg=numel(varargin) + 1;
 assert(Narg>1,'Error: report takes at least 2 NTOF objects as input')
 
 %% create figure with three subplots
-figure('Units','Inches','Position',[7,6,10,5],'Color','w',...
+figure('Units','Inches','Position',[4,6,14,6],'Color','w',...
     'PaperPositionMode','auto')
 
 subplot('Position',[0.075, 0.15, 0.5, 0.8])
@@ -33,6 +33,8 @@ xlabel('Detector')
 ylabel('Bang Time [ns]')
 
 if Narg == 6
+    Fsig = zeros(6,1);
+    Fsig(1) = object.Settings.FitSignal;
     Tion = zeros(Narg,1);
     bangtime = zeros(Narg,1);
     
@@ -44,12 +46,15 @@ if Narg == 6
     line(X,object.Settings.FinalSignal.Data,'Color','b',...
         'LineWidth',1,'LineStyle','-',...
         'Parent',ax1)
-    line(X,evaluate(object.Settings.Fit,X),'Color','r',...
-        'LineWidth',1,'LineStyle','--',...
-        'Parent',ax1)
+    if ~isnan(Fsig(1))
+        line(X,evaluate(object.Settings.Fit,X),'Color','r',...
+            'LineWidth',1,'LineStyle','--',...
+            'Parent',ax1)
+    end
     % plot remaining objects
     
     for n=1:Narg-1
+        Fsig(n+1) =  varargin{n}.Settings.FitSignal;
         X = varargin{n}.Settings.FinalSignal.Grid;
         Tion(n+1) = varargin{n}.Settings.Fit.Parameter{1}(2);
         bangtime(n+1) = 1e9*varargin{n}.Settings.Fit.Parameter{1}(1);
@@ -57,37 +62,41 @@ if Narg == 6
         line(X,varargin{n}.Settings.FinalSignal.Data,'Color','b',...
             'LineWidth',1,'LineStyle','-',...
             'Parent',ax1)
-        line(X,evaluate(varargin{n}.Settings.Fit,X),'Color','r',...
-            'LineWidth',1,'LineStyle','--',...
-            'Parent',ax1)
+        if ~isnan(Fsig(n+1))
+            line(X,evaluate(varargin{n}.Settings.Fit,X),'Color','r',...
+                'LineWidth',1,'LineStyle','--',...
+                'Parent',ax1)
+        end
     end
+    mask = ~isnan(Fsig);
     
-    Tstats = PlotMeanStd(Tion,ax2);
-    BTstats = PlotMeanStd(bangtime-3000,ax3);
+    Tstats = PlotMeanStd(Tion,ax2,mask);
+    BTstats = PlotMeanStd(bangtime-3000,ax3,mask);
     
     text(3.66e-6, 0.8,sprintf('$T_i=%3.1f~+/- %3.1f$ keV',Tstats(1),Tstats(2)),...
-    'Interpreter','latex','Parent',ax1,'FontSize',12)
+        'Interpreter','latex','Parent',ax1,'FontSize',12)
     text(3.66e-6, 0.7,sprintf('$t_{bang}=%3.1f~+/- %3.1f$ ns',BTstats(1)+3000,BTstats(2)),...
-    'Interpreter','latex','Parent',ax1,'FontSize',12)
-
-end
-if nargout == 2
-   varargout{1} = Tstats;
-   varargout{2} = BTstats + 3000;
-else
-if nargout == 3
-   varargout{1} = Tstats;
-   varargout{2} = BTstats + [3000, 0];
-   varargout{3} = [ax1, ax2, ax3];
+        'Interpreter','latex','Parent',ax1,'FontSize',12)
     
 end
+if nargout == 2
+    varargout{1} = Tstats;
+    varargout{2} = BTstats + 3000;
+else
+    if nargout == 3
+        varargout{1} = Tstats;
+        varargout{2} = BTstats + [3000, 0];
+        varargout{3} = [ax1, ax2, ax3];
+        
+    end
 end
-    function stats = PlotMeanStd(vals, ax)
-        meanval = mean(vals);
-        sigmaval = std(vals);
+    function stats = PlotMeanStd(vals, ax, mask)
+        detectors = [1, 2, 3, 4, 5, 6];
+        meanval = mean(vals(mask));
+        sigmaval = std(vals(mask));
         xvals = get(ax,'XLim');
         
-        bar(ax,[1, 2, 3, 4, 5, 6], vals,'FaceColor','r')
+        bar(ax,detectors(mask), vals(mask),'FaceColor','r')
         line(xvals,[meanval meanval],'Color','k','Parent',ax)
         line(xvals,[meanval+sigmaval meanval+sigmaval],'Color','k','LineStyle','--','Parent',ax)
         line(xvals,[meanval-sigmaval meanval-sigmaval],'Color','k','LineStyle','--','Parent',ax)
