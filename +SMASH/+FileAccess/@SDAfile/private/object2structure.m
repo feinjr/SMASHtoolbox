1132c51a161163478e9a16253fdf7c0630710b1b
Daniel Dolan <dhdolan@sandia.gov>
2016-03-03 20:28:13 -0700

    Update SDA restore    -Classes lacking a restore method (e.g., most of MATLAB's builtin classes) may now be restored from an archive.    -More detailed warnings indicate when private/protected data is not transferred.

--- +SMASH/+FileAccess/@SDAfile/private/object2structure.m
+++ +SMASH/+FileAccess/@SDAfile/private/object2structure.m
@@ -1,15 +1,11 @@
 function data=object2structure(object)
 
-%warning off MATLAB:structOnObject
-%data=struct(object);
-%warning on MATLAB:structOnObject
-
 data=struct();
 temp=metaclass(object);
 name=temp.PropertyList;
 for k=1:numel(name)
     try
-    data.(name(k).Name)=object.(name(k).Name);
+        data.(name(k).Name)=object.(name(k).Name);
     catch
         % do nothing
     end
