a4123d43fc7f17b7fde5ec7c24acd3200139b0ed
Tommy Ao <tao@sandia.gov>
2016-04-21 14:32:26 -0600

    Image bandpass method - improved and default to not show power spectra

--- +SMASH/+ImageAnalysis/@Image/bandpass.m
+++ +SMASH/+ImageAnalysis/@Image/bandpass.m
@@ -1,13 +1,15 @@
 % BANDPASS Bandpass filtering of Image data 
 %
 % Usage:
-%   >> object=bandpass(object,range,type,[order]);
+%   >> object=bandpass(object,range,type,[order],[spectra]);
 % "range" are normalized low & high frequency cutoffs (0<value<1), 
 % e.g. [0.001 0.25];
 % "type" is for the following filters:
 %   'ideal', 'gaussian', 'butterworth', 'chebyshev'; 
 % "order" is an integer>=1 used for 'butterworth' & 'chebyshev' filters    
-%
+% "spectra" is to used to show power spectra:
+%   'on' or 'off' (default)
+% 
 % See also IMAGE
 
 % created July 24, 2014 by Tommy Ao (Sandia National Laboratories)
@@ -34,6 +36,11 @@ if nargin>3 && ~isempty(varargin{3}) && isnumeric(varargin{3})
 else
     order=1;
 end
+if nargin>4 && ~isempty(varargin{4}) && ischar(varargin{4}) 
+    spectra=varargin{4};
+else
+    spectra='off';
+end
 
 % verify uniform grid
 object=makeGridUniform(object);
@@ -42,44 +49,55 @@ object=makeGridUniform(object);
 m = max(size(object.Data)); % maximum dimension of data
 P = 2^nextpow2(2*m); % find power-of-2 at least twice m
 
-% set up the meshgrid arrays needed for computing the required distances
-[D,D0H,D0L]=setupmeshgrid(P,range);
-
-% calculate bandpass type
-[B,H1,H2]=bandpasstype(D,D0H,D0L,type,order);
-
-% FFT data
-F=fft2(double(object.Data),H1,H2);
-
-% apply bandpass to FFT data
-BF = B.*F;
-
-% inverse bandpassed FFT data
-BFdata=real(ifft2(BF)); 
-BFdata=BFdata-min(min(BFdata));
+% calculate bandpassed data
+BFdata=calculate_bandpass(object.Data,P,range,type,order,spectra);
 
 % create bandpassed Image object
 object.Data=BFdata(1:length(object.Grid2),1:length(object.Grid1));
 object.DataLabel='Intensity (a.u.)';
 object=updateHistory(object);
-view(object);
-title(strcat((object.GraphicOptions.Title),' - bandpassed'));
 
-% plot power spectra
-figure; set(gcf,'OuterPosition',[0 500 1000 1000]);
-subplot(2,2,1); 
-imagesc(fftshift(log(abs(F)))); caxis([0 max(max(log(abs(F))))]);
-title(strcat((object.GraphicOptions.Title),' - power spectrum'));
-subplot(2,2,2);
-imagesc(fftshift(B)); caxis([min(min(abs(B))) max(max(abs(B)))]);
-title('Bandpass power spectrum');
-subplot(2,2,3);
-imagesc(fftshift(log(abs(BF)))); caxis([0 max(max(log(abs(BF))))]);
-title(strcat((object.GraphicOptions.Title),' - bandpassed power spectrum'));
+% show bandpassed Image object
+% view(object);
+% title(strcat((object.GraphicOptions.Title),' - bandpassed'));
 
 end
 
-function [D,D0H,D0L]=setupmeshgrid(P,range)
+function BFdata=calculate_bandpass(objectData,P,range,type,order,spectra)
+
+% FFT bandpass type
+[B,H1,H2]=bandpasstype(P,range,type,order);
+
+% FFT data
+F=fft2(double(objectData),H1,H2);
+
+% apply bandpass to data
+BF = B.*F;
+
+% inverse FFT bandpassed data
+BFdata=real(ifft2(BF)); 
+BFdata=BFdata-min(min(BFdata));
+
+if strcmpi(spectra,'on')
+    % plot power spectra
+    figure;
+    subplot(2,2,1); 
+    imagesc(fftshift(log(abs(F)))); caxis([0 max(max(log(abs(F))))]);
+    title('Power spectrum of data');
+    subplot(2,2,2);
+    imagesc(fftshift(B)); caxis([min(min(abs(B))) max(max(abs(B)))]);
+    title('Power spectrum of bandpass');
+    subplot(2,2,3);
+    imagesc(fftshift(log(abs(BF)))); caxis([0 max(max(log(abs(BF))))]);
+    title('Power spectrum of bandpassed data');
+    set(gcf,'OuterPosition',[0 300 800 800]);
+end
+
+end
+
+
+function [B,H1,H2]=bandpasstype(P,range,type,order)
+
 % set up the meshgrid arrays needed for computing the required distances
 PQ = [P, P];
 
@@ -96,11 +114,7 @@ v(idy) = v(idy) - PQ(2);
 [V, U] = meshgrid(v, u); % compute the meshgrid arrays
 D = sqrt(U.^2 + V.^2); % compute the distances D(U, V)
 
-end
-
-function [B,H1,H2]=bandpasstype(D,D0H,D0L,type,order)
-
-% calculte bandpass type
+% calculate bandpass type
 switch lower(type)
     case 'ideal'
         L = double(D <=D0L);
