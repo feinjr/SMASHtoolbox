c79f60c6743be944ad810255e44ccff9cfef6934
Paul Specht <pespech@S961998.srn.sandia.gov>
2016-10-27 09:10:03 -0600

    Modification to VISAR view method to enable viewing multiple VISAR objects

--- +SMASH/+Velocimetry/@VISAR/view.m
+++ +SMASH/+Velocimetry/@VISAR/view.m
@@ -1,11 +1,10 @@
-% VIEW - Display a VISAR object
-% 
-% This method plots the various Signal objects and Signal Group objects
-% of a VISAR object.  All possible syntaxes are below.
-%     >> view(object);
-%     >> view(object,Signal); 
+% VIEW - Display a property of VISAR objects
 %
-% There are a variety of Signal options
+% This method plots the various Signal objects and Signal Group objects
+% of the specified VISAR objects.  The syntax is below
+%     >> view(object1,object2,...,objectN,'Signal',string,'PlotMethod',string,'Target',handle);
+%
+% There are several Signal options:
 %     'Measurement'  - View the Raw measurement signals.  This is the
 %                      default.
 %     'Experiment'   - View the Experimental region of the raw signals.
@@ -23,113 +22,271 @@
 %                      be analyzed for this option.
 %     'Velocity'     - View the Velocity signal.  The VISAR object must
 %                      be analyzed for this option.
-% 
+%
+% There are two PlotMethods:
+%      'Sub'         - Plots the object signals in its own axis as a
+%                      subplot of the figure.  This is the default for
+%                      multiple VISAR objects.
+%      'Single'      - Plots all the object signals on a signal axis.  This
+%                      is the default for a single VISAR object.
+%
+% Target is used to specify an axis handle for plotting onto an exisitng 
+% figure. This option is only avaible for the 'Single' PlotMethod.  If no
+% target is specified a new figure is generated.
+%
 % created March 10, 2016 by Paul Specht (Sandia National Laboratories)
 %
-function varargout=view(object,mode,target)
+function varargout=view(varargin)
 
-% manage input
-if (nargin<2) || isempty(mode)
-    mode='measurement';
-end
-assert(ischar(mode),'ERROR: Invalid View Mode');
-
-if (nargin<3) || isempty(target)
-    target=[];
-end
-
-% generate plot
-n=object.Measurement.NumberSignals;
-if n > 3
-    label={'D1A','D1B','D2A','D2B'};
-elseif n > 2
-    label={'D1','D2','BIM'};
+% manage the input
+mode='measurement';
+target=[];
+if nargin < 2
+    method='single';
 else
-    label={'D1','D2'};
+    method='sub';
 end
-switch lower(mode)
-    case 'measurement'
-        h=view(object.Measurement,target);
-        ylabel('Amplitude');
-        xlabel('Time');
-        legend(label,'Location','best');
-    case 'processed'
-        assert(isa(object.Processed,'SMASH.SignalAnalysis.SignalGroup'),...
-            'ERROR: Must Analyze Signal to View');
-        h=view(object.Processed,target);
-        ylabel('Amplitude');
-        xlabel('Time');
-        legend(label,'Location','best');
-    case 'experiment'
-        if isa(object.Processed,'SMASH.SignalAnalysis.SignalGroup')
-            h=view(object.Processed,target);
-        else
-            h=view(object.Measurement,target);
+
+counter=1;
+while counter <= nargin
+    if isa(varargin{counter},'SMASH.Velocimetry.VISAR');
+        objs(counter)=counter;
+        counter=counter+1;
+    elseif ischar(varargin{counter})
+        switch lower(varargin{counter})
+            case 'signal'
+                if counter+1 <= nargin
+                    mode=varargin{counter+1};
+                    assert(ischar(mode),'ERROR: Invalid Signal');
+                else
+                    error('ERROR: No Signal Specified');
+                end
+            case 'plotmethod'
+                if counter+1 <= nargin
+                    method=varargin{counter+1};
+                    assert(ischar(method),'ERROR: Invalid PlotMethod');
+                else
+                    error('ERROR: No PlotMethod Specified');
+                end
+            case 'target'
+                if counter+1 <= nargin
+                    target=varargin{counter+1}(1);
+                    method='single';
+                    assert(isgraphics(target),'ERROR: Invalid target');
+                else
+                    error('ERROR: No Target Specified');
+                end
+            otherwise
+                error('ERROR: Invalid Input Designation');
         end
-        xlim(object.ExperimentalRegion);
-        ylabel('Amplitude');
-        xlabel('Time');
-        legend(label,'Location','best');
-    case 'reference'
-        assert(length(object.ReferenceRegion)==2,...
-            'ERROR: Must Define a Reference Region');
-        if isa(object.Processed,'SMASH.SignalAnalysis.SignalGroup')
-            h=view(object.Processed,target);
+        counter=counter+2;
+    else
+        error('ERROR: Invalid Input Designation');
+    end
+end
+
+if isempty(objs)
+    error('ERROR: No VISAR object defined');
+elseif length(objs) == 1
+    method='single';
+end
+
+switch lower(method)
+    case 'single'
+        fig=SMASH.MUI.Figure;
+        fig.Hidden=true;
+        hand=fig.Handle;
+        target(1:length(objs))=axes('Parent',hand);
+    case 'sub'
+        dim=ceil(sqrt(length(objs)));
+        if dim*dim-length(objs) >= dim
+            adjust=1;
         else
-            h=view(object.Measurement,target);
+            adjust=0;
         end
-        xlim(object.ReferenceRegion);
+        fig=SMASH.MUI.Figure;
+        fig.Hidden=true;
+        for m=1:length(objs)
+            target(m)=subplot(dim,dim-adjust,m);
+        end
+    otherwise
+        error('ERROR: Invalid PlotMethod');
+end
+
+%plot out the signals
+label={};
+color=lines(4*length(objs));
+for k=1:length(objs)
+    object=varargin{objs(k)};
+    switch lower(mode)
+        case 'measurement'
+            h=view(object.Measurement,[],target(k));
+            type=1;
+            label=getLabel(object,label,type);
+            setColor(h,color,type,k);             
+        case 'processed'
+            assert(isa(object.Processed,'SMASH.SignalAnalysis.SignalGroup'),...
+                'ERROR: Must Process Signal to View');
+            h=view(object.Processed,[],target(k));
+            type=1;
+            label=getLabel(object,label,type);
+            setColor(h,color,type,k);
+        case 'experiment'
+            if isa(object.Processed,'SMASH.SignalAnalysis.SignalGroup')
+                tempobj=crop(object.Processed,object.ExperimentalRegion);
+            else
+                tempobj=crop(object.Measurement,object.ExperimentalRegion);
+            end
+            h=view(tempobj,[],target(k));
+            type=-1;
+            label=getLabel(object,label,1);
+            setColor(h,color,type,k);
+        case 'reference'
+            assert(length(object.ReferenceRegion)==2,...
+                'ERROR: Must Define a Reference Region');
+            if isa(object.Processed,'SMASH.SignalAnalysis.SignalGroup')
+                tempobj=crop(object.Processed,object.ReferenceRegion);
+            else
+                tempobj=crop(object.Measurement,object.ReferenceRegion);
+            end
+            h=view(tempobj,[],target(k));
+            type=-1;
+            label=getLabel(object,label,1);
+            setColor(h,color,type,k);
+        case 'fringeshift'
+            assert(isa(object.FringeShift,'SMASH.SignalAnalysis.Signal'),...
+                'ERROR: Must Process Signal to View');
+            h=view(object.FringeShift,target(k));
+            type=4;
+            label=getLabel(object,label,type);
+            setColor(h,color,type,k);
+        case 'contrast'
+            assert(isa(object.Contrast,'SMASH.SignalAnalysis.Signal'),...
+                'ERROR: Must Process Signal to View');
+            h=view(object.Contrast,target(k));
+            type=5;
+            label=getLabel(object,label,type);
+            setColor(h,color,type,k);
+        case 'lissajou'
+            assert(isa(object.Quadrature,'SMASH.SignalAnalysis.SignalGroup'),...
+                'ERROR: Must Process Signal to View');
+            h=line(object.Quadrature.Data(:,1),object.Quadrature.Data(:,2),'Parent',target(k));
+            apply(object.Quadrature.GraphicOptions,h);
+            type=2;
+            label=getLabel(object,label,type);
+            setColor(h,color,type,k);
+        case 'quadrature'
+            assert(isa(object.Quadrature,'SMASH.SignalAnalysis.SignalGroup'),...
+                'ERROR: Must Process Signal to View');
+            h=view(object.Quadrature,[],target(k));
+            type=3;
+            label=getLabel(object,label,type);
+            setColor(h,color,type,k);
+        case 'velocity'
+            assert(isa(object.Velocity,'SMASH.SignalAnalysis.Signal'),...
+                'ERROR: Must Analyze Signal to View');
+            h=view(object.Velocity,target(k));
+            type=6;
+            label=getLabel(object,label,type);
+            setColor(h,color,type,k);
+        otherwise
+            error('ERROR: Invalid View Mode');
+    end
+    if type < 2
         ylabel('Amplitude');
         xlabel('Time');
-        legend(label,'Location','best');
-    case 'fringeshift'
-        assert(isa(object.FringeShift,'SMASH.SignalAnalysis.Signal'),...
-            'ERROR: Must Analyze Signal to View');
-        h=view(object.FringeShift,target);
-        ylabel('Phase');
-        xlabel('Time');
-    case 'contrast'
-        assert(isa(object.Contrast,'SMASH.SignalAnalysis.Signal'),...
-            'ERROR: Must Analyze Signal to View');
-        h=view(object.Contrast,target);
-        ylabel('Amplitude');
-        xlabel('Time');
-    case 'lissajou'
-        assert(isa(object.Quadrature,'SMASH.SignalAnalysis.SignalGroup'),...
-            'ERROR: Must Analyze Signal to View');
-        target=SMASH.MUI.Figure;
-        target=target.Handle;
-        target=axes('Parent',target);
-        axes(target);
-        h=line(object.Quadrature.Data(:,1),object.Quadrature.Data(:,2),'Parent',target);
-        apply(object.Quadrature.GraphicOptions,h);
+        if length(unique(target)) > 1
+            NN=object.Measurement.NumberSignals;
+            legend(label{NN*(k-1)+1:NN*k},'Location','best');
+        end
+    elseif type == 2
         theta=linspace(0,2*pi,200);
         x=object.EllipseParameters(1)+object.EllipseParameters(3)*cos(theta);
         y=object.EllipseParameters(2)+object.EllipseParameters(4)*sin(theta-object.EllipseParameters(5));
-        line(x,y,'Color',[1 0 0],'LineWidth',2);
         ylabel('Amplitude');
         xlabel('Amplitude');
-        legend('Lissajou','Elliptical Fit','Location','best');
-    case 'quadrature'
-        assert(isa(object.Quadrature,'SMASH.SignalAnalysis.SignalGroup'),...
-            'ERROR: Must Analyze Signal to View');
-        h=view(object.Quadrature,target);
+        if length(unique(target)) > 1
+            axes(target(k));
+            line(x,y,'Color',[0 0 0],'LineWidth',2);
+            labeltemp={label{k},'Elliptical Fit'};
+            legend(labeltemp,'Location','best');
+        end
+    elseif type == 3
         ylabel('Amplitude');
         xlabel('Time');
-        legend('DX','DY','Location','best');
-    case 'velocity'
-        assert(isa(object.Velocity,'SMASH.SignalAnalysis.Signal'),...
-            'ERROR: Must Analyze Signal to View');
-        h=view(object.Velocity,target);
+        if length(unique(target)) > 1
+            legend(label{2*(k-1)+1:2*k},'Location','best');
+        end
+    elseif type == 4
+        ylabel('Phase');
+        xlabel('Time');
+        if length(unique(target)) > 1
+            legend(label{k},'Location','best');
+        end
+    elseif type == 5
+        ylabel('Amplitude');
+        xlabel('Time');
+        if length(unique(target)) > 1
+            legend(label{k},'Location','best');
+        end
+    elseif type == 6
         xlabel('Time');
         ylabel('Velocity');
-    otherwise
-        error('ERROR: Invalid View Mode');
+        if length(unique(target)) > 1
+            legend(label{k},'Location','best');
+        end
+    end
 end
+if length(unique(target)) == 1
+    if type == 2
+        line(x,y,'Color',[0 0 0],'LineWidth',2);
+        labeltemp=[label,'Elliptical Fit'];
+        legend(labeltemp,'Location','best');
+    else
+        legend(label,'Location','best');
+    end
+end
+
+
+fig.Hidden=false;
 
 % manage output
 if nargout>0
     varargout{1}=h;
-end   
+end
+
+    function Label=getLabel(object,label,type)
+        if type < 2
+            %determine the number of signals
+            N=object.Measurement.NumberSignals;
+            if N > 3
+                l={['D1A-',object.Label],['D1B-',object.Label],['D2A-',object.Label],['D2B-',object.Label]};
+            elseif N > 2
+                l={['D1-',object.Label],['D2-',object.Label],['BIM-',object.Label]};
+            else
+                l={['D1-',object.Label],['D2-',object.Label]};
+            end
+        elseif type == 2
+            l={['Lissajou-',object.Label]};
+        elseif type == 3
+            l={['DX-',object.Label],['DY-',object.Label]};
+        elseif type > 3
+            l={object.Label};
+        end
+        Label=[label,l];
+    end
+
+    function setColor(handle,Colors,type,k)
+        if type < 2
+            int=4;
+        elseif type == 3
+            int=2;
+        else
+            int=1;
+        end
+        for n=1:length(handle)
+            set(handle(n),'Color',Colors(n+int*(k-1),:));
+        end
+    end
 
 end
