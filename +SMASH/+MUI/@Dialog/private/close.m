542f8976002046679ed68aa9dc3feef349ed83e6
Dolan, Daniel H <dhdolan@sandia.gov>
2017-03-02 09:30:54 -0700

    Updated SDA attribute management for better compatibility with other computer languages.

--- +SMASH/+MUI/@Dialog/private/close.m
+++
@@ -1,8 +0,0 @@
-
-%%
-function close(obj)
-% dialog.close : close the dialog
-%   >> object.close;
-delete(obj.Handle);
-obj.Handle=[];
-end
--- +SMASH/+MUI/@Dialog/private/verify.m
+++
@@ -1,9 +0,0 @@
-
-function verify(object)
-
-if ~isvalid(object) || ~ishandle(object.Handle)
-    delete(object);
-    error('ERROR: dialog box no longer exists');
-end
-
-end
