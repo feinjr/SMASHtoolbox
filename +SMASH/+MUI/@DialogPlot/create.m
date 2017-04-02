d6576b7cd3d362a71c30e95978f6e2dfe12cc2fd
Dolan, Daniel H <dhdolan@sandia.gov>
2017-04-02 06:44:24 -0600

    Created DialogPlot class

---
+++ +SMASH/+MUI/@DialogPlot/create.m
@@ -0,0 +1,67 @@
+function object=create(object,varargin)
+
+%%
+temp=SMASH.MUI.Figure('NumberTitle','off','Name',object.PrivateName);
+object.Figure=temp.Handle;
+try
+    set(object.Figure,'SizeChangedFcn',@resizeFigure);
+catch
+    set(object.Figure,'ResizeFcn',@resizeFigure);
+end
+    function resizeFigure(varargin)
+        %
+        OldUnits=get(object.Figure,'Units');
+        set(object.Figure,'Units','pixels');
+        set(object.ControlPanel,'Units','pixels');
+        set(object.PlotPanel,'Units','pixels');
+        %
+        FigPosition=get(object.Figure,'Position');
+        ControlPosition=get(object.ControlPanel,'Position');       
+        minwidth=2*ControlPosition(3);                
+        if FigPosition(3) < minwidth
+            FigPosition(3) = minwidth;
+        end
+        minheight=ControlPosition(4);
+        if FigPosition(4) < minheight
+            change=minheight-FigPosition(4);
+            y0=FigPosition(2);
+            FigPosition(2) = y0-change;
+            FigPosition(4) = minheight;
+        end
+        set(object.Figure,'Position',FigPosition);
+        %
+        ControlPosition(1)=0;
+        ControlPosition(2)=FigPosition(4)-ControlPosition(4);
+        set(object.ControlPanel,'Position',ControlPosition);        
+        %
+        PlotPosition=FigPosition;
+        PlotPosition(1)=ControlPosition(3);
+        PlotPosition(3)=PlotPosition(3)-PlotPosition(1);
+        PlotPosition(2)=0;
+        set(object.PlotPanel,'Position',PlotPosition);
+        %
+        set(object.Figure,'Units',OldUnits);
+    end
+
+%%
+object.ControlPanel=uipanel(...
+    'Units','normalized','Position',[0 0 0.20 1],...
+    'BorderType','none','HandleVisibility','callback');
+
+object.PlotPanel=uipanel(...
+    'Units','normalized','Position',[0.20 0 0.80 1],...
+    'BorderType','none');
+axes('Parent',object.PlotPanel,'Box','on');
+
+%%
+if nargin > 1
+    set(object.Figure,varargin{:});
+end
+
+%%
+object.DialogBox=SMASH.MUI.Dialog();
+object.DialogBox.Hidden=true;
+
+setappdata(object.Figure,'DialogPlot',object);
+
+end
